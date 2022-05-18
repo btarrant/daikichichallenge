028b61c1e3ed0f06e706fe410c1be080b900842c
Brandon Tarrant <brandontarrant04@gmail.com>
2022-05-18 13:34:41 -0400

    first commit

---
+++ src/main/java/com/brandontarrant/daikichichallenge/DaikichichallengeApplication.java
@@ -0,0 +1,13 @@
+package com.brandontarrant.daikichichallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichichallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichichallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brandontarrant/daikichichallenge/DaikichichallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandontarrant.daikichichallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichichallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
