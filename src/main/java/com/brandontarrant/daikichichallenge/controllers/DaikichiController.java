99b5176b69974519edfd02bf7235fd145a339992
Brandon Tarrant <brandontarrant04@gmail.com>
2022-05-18 14:04:19 -0400

    Challenge part 2.

--- src/main/java/com/brandontarrant/daikichichallenge/controllers/DaikichiController.java
+++ src/main/java/com/brandontarrant/daikichichallenge/controllers/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.brandontarrant.daikichichallenge.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,21 @@ public class DaikichiController {
 	@RequestMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@RequestMapping("/travel/{city}")
+	public String travel(@PathVariable String city) {
+		return "Congratulations! You will soon travel to " + city;
+	}
+	
+	@RequestMapping("/lotto/{number}")
+	public String lotto(@PathVariable int number) {
+		
+		if (number % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		}
+		else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
 	}
 }
