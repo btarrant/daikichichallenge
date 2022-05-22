3a4af1ac4246a58cc49f31d2577860bdbeb33cb9
Brandon Tarrant <brandontarrant04@gmail.com>
2022-05-21 23:45:42 -0400

    Finished the project.

--- src/main/java/com/brandontarrant/daikichichallenge/controllers/DaikichiController.java
+++ src/main/java/com/brandontarrant/daikichichallenge/controllers/DaikichiController.java
@@ -1,46 +1,15 @@
 package com.brandontarrant.daikichichallenge.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpSession;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
-@RequestMapping("/daikichi")
 public class DaikichiController {
-	
-	@RequestMapping("")
-	public String daikichi() {
-		return "Welcome";
-	}
-	
-	@RequestMapping("/today")
-	public String today() {
-		return "Today you will find luck in all your endeavors";
-	}
-	
-	@RequestMapping("/tomorrow")
-	public String tomorrow() {
-		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
-	}
-	
-	@RequestMapping("/travel/{city}")
-	public String travel(@PathVariable String city) {
-		return "Congratulations! You will soon travel to " + city;
-	}
-	
-	@RequestMapping("/lotto/{number}")
-	public String lotto(@PathVariable int number) {
-		
-		if (number % 2 == 0) {
-			return "You will take a grand journey in the near future, but be weary of tempting offers.";
-		}
-		else {
-			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
-		}
-	}
 	
 	@GetMapping("/")
 	public String index() {
@@ -51,4 +20,48 @@ public class DaikichiController {
 	public String omikuji() {
 		return "index.jsp";
 	}
+	
+	@GetMapping("/omikuji/show")
+	public String show(HttpSession Session, Model model) {
+		
+		String result = (String) Session.getAttribute("omikujiPost");
+		model.addAttribute("result", result);
+		
+		return "showOmikuji.jsp";
+	}
+	
+	@PostMapping("/processFormData")
+	public String process(
+			@RequestParam("number") int number,
+			@RequestParam("city") String city,
+			@RequestParam("person") String person,
+			@RequestParam("hobby") String hobby,
+			@RequestParam("thing") String thing,
+			@RequestParam("message") String message,
+			HttpSession session
+			) {
+		
+		String omikujiPost = String.format(
+				"In %s years, you will live in %s with %s as your roommate, %s. The next time you see a %s, you will have good luck. Also, %s",
+				number, city, person, hobby, thing, message);
+		
+		session.setAttribute("omikujiPost", omikujiPost);
+		
+		return "redirect:omikuji/show";
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/webapp/WEB-INF/index.jsp
+++ src/main/webapp/WEB-INF/index.jsp
@@ -14,32 +14,40 @@
 
 	<h1>Send an Omikuji!</h1>
 	
-	<form action="">
+	<form action="/processFormData" method="post">
 	
 		<div>
 			<label for="">Pick any number from 5 to 25</label>
-			<input type="number" />
+			<input type="number" name="number"/>
 		</div>
 		
 		<div>
 			<label for="">Enter the name of any city</label>
-			<input type="text" />
+			<input type="text" name="city"/>
 		</div>
 		
 		<div>
 			<label for="">Enter the name of any real person</label>
-			<input type="text" />
+			<input type="text" name="person"/>
 		</div>
 		
 		<div>
 			<label for="">Enter professional endeavor or hobby</label>
-			<input type="text" />
+			<input type="text" name="hobby"/>
+		</div>
+		
+		<div>
+			<label for="">Enter any type of living thing</label>
+			<input type="text" name="thing"/>
 		</div>
 		
 		<div>
 			<label for="">Say something nice to someone</label>
-			<textarea name="" id="" cols="30" rows="10"></textarea>	
+			<textarea name="message" id="" cols="30" rows="10"></textarea>	
 		</div>
+		
+		<p>Send and show a friend!</p>
+		<input type="submit" value="Send"/>
 	
 	</form>
 		
